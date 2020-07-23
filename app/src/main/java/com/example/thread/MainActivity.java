5e9a567ecd87a67aca92272b517f65db8eb48905
yangchen <chen.yang1@thoughtworks.com>
2020-07-23 17:43:42 +0800

    add onclick event to button thread practices

--- app/src/main/java/com/example/thread/MainActivity.java
+++ app/src/main/java/com/example/thread/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.thread;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.thread)
+    Button mButtonThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.thread)
+    void onClick() {
+        Intent intent = new Intent(this, ThreadActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/thread/ThreadActivity.java
@@ -0,0 +1,14 @@
+package com.example.thread;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+    }
+}
