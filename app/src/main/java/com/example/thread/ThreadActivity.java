708dbdc12bf812bd9bbcff326fbf2a12dcf791f4
yangchen <chen.yang1@thoughtworks.com>
2020-07-23 17:48:20 +0800

    modify button text by thread

--- app/src/main/java/com/example/thread/ThreadActivity.java
+++ app/src/main/java/com/example/thread/ThreadActivity.java
@@ -1,14 +1,38 @@
 package com.example.thread;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class ThreadActivity extends AppCompatActivity {
+
+    @BindView(R.id.start)
+    Button mButtonStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thread);
+
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.start) void onClick(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                mButtonStart.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mButtonStart.setText("hello");
+                    }
+                });
+            }
+        }).start();
     }
 }
